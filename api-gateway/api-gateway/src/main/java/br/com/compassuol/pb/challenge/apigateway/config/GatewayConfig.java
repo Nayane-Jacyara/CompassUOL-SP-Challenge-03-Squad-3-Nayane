8302f5bd7db150b27e2e765ed073bd3ffa696778
Nayane-Jacyara <njcs@discente.ifpe.edu.br>
2023-07-10 04:14:29 -0300

    feat: building the gateway

---
+++ api-gateway/api-gateway/src/main/java/br/com/compassuol/pb/challenge/apigateway/config/GatewayConfig.java
@@ -0,0 +1,33 @@
+package br.com.compassuol.pb.challenge.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("users", r -> r
+                        .path("/users")
+                        .uri("http://localhost:8082/)"))
+                .route("products", r -> r
+                        .path("/products")
+                        .uri("http://localhost:8083/)"))
+                .route("oauth", r -> r
+                        .path("/oauth")
+                        .uri("http://localhost:8084/)"))
+                .route("email", r -> r
+                        .path("/sending-email")
+                        .uri("http://localhost:8085/)"))
+                .route("products", r -> r
+                        .path("/products/**")
+                        .filters(f -> f.rewritePath("/products/(?<params>.*)", "/?page=1&linesPerPage=5&direction=DESC&orderBy=name"))
+                        .uri("http://localhost:8083/"))
+
+                .build();
+    }
+}
