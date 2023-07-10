1437fd15d44ba38af28cec4c6adc5203cb41e2bc
Nayane-Jacyara <njcs@discente.ifpe.edu.br>
2023-07-10 15:59:13 -0300

    fix: database fixing

--- database tables/creating data.sql
+++ database tables/creating data.sql
@@ -1,4 +1,3 @@
-use ProductManagementSystem; 
 
 -- Inserting categories
 INSERT INTO tb_category (name) VALUES ('Electronics');
@@ -6,10 +5,9 @@ INSERT INTO tb_category (name) VALUES ('Clothing');
 INSERT INTO tb_category (name) VALUES ('Home and Kitchen');
 
 -- Ininserting users
-INSERT INTO tb_user (firstName, lastName, email, password, roles) VALUES ('John', 'Doe', 'john@gmail.com', 'john', 'ROLE_USER');
-INSERT INTO tb_user (firstName, lastName, email, password, roles) VALUES ('Jane', 'Smith', 'jane@gmail.com', 'jane', 'ROLE_USER');
-INSERT INTO tb_user (firstName, lastName, email, password, roles) VALUES ('Nayane', 'Jacyara', 'njcs@discente.ifpe.edu.br
-', 'admin123', 'ROLE_ADMIN');
+INSERT INTO tb_user (first_name, last_name, email, password, roles) VALUES ('John', 'Doe', 'john@gmail.com', 'john', 'ROLE_USER');
+INSERT INTO tb_user (first_name, last_name, email, password, roles) VALUES ('Jane', 'Smith', 'jane@gmail.com', 'jane', 'ROLE_USER');
+INSERT INTO tb_user (first_name, last_name, email, password, roles) VALUES ('Nayane', 'Jacyara', 'njcs@discente.ifpe.edu.br', 'admin123', 'ROLE_ADMIN');
 
 -- Inserting products
 INSERT INTO tb_product (date, description, name, imgUrl, price, category_id) VALUES ('2023-07-01 10:00:00', 'Smartphone with advanced features', 'iPhone 12', 'https://a-static.mlcdn.com.br/450x450/iphone-12-apple-128gb-roxo-tela-61-12mp-ios/magazineluiza/231148900/6c9edfdf13d754062abdb657e863721c.jpg', 3999.99, 1);
@@ -27,4 +25,6 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 2);
 -- Relating users and roles
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2); -- Admin é atribuído a ROLE_ADMIN
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+
+
--- database tables/creation of tables.sql
+++ database tables/creation of tables.sql
@@ -1,49 +1,47 @@
-CREATE DATABASE ProductManagementSystem;
+CREATE DATABASE IF NOT EXISTS ProductManagementSystem;
 
-use ProductManagementSystem; 
-
-CREATE TABLE tb_category (
+CREATE TABLE IF NOT EXISTS tb_category (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tb_user (
+CREATE TABLE IF NOT EXISTS tb_user (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  firstName VARCHAR(50) NOT NULL,
-  lastName VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(20) NOT NULL,
   roles VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE tb_product (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  date DATETIME NOT NULL,
-  description TEXT NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  imgUrl VARCHAR(255),
-  price DECIMAL(10, 2) NOT NULL,
-  category_id INT,
-  FOREIGN KEY (category_id) REFERENCES tb_category(id)
+CREATE TABLE IF NOT EXISTS tb_product (
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   date DATETIME NOT NULL,
+   description TEXT NOT NULL,
+   name VARCHAR(50) NOT NULL,
+   imgUrl VARCHAR(255),
+   price DECIMAL(10, 2) NOT NULL,
+   category_id INT,
+   FOREIGN KEY (category_id) REFERENCES tb_category(id)
 );
 
-CREATE TABLE tb_role (
+CREATE TABLE IF NOT EXISTS tb_role (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tb_product_category (
-  product_id INT,
-  category_id INT,
-  FOREIGN KEY (product_id) REFERENCES tb_product(id),
-  FOREIGN KEY (category_id) REFERENCES tb_category(id),
-  PRIMARY KEY (product_id, category_id)
+CREATE TABLE IF NOT EXISTS tb_product_category (
+   product_id INT,
+   category_id INT,
+   FOREIGN KEY (product_id) REFERENCES tb_product(id),
+   FOREIGN KEY (category_id) REFERENCES tb_category(id),
+   PRIMARY KEY (product_id, category_id)
 );
 
-CREATE TABLE tb_user_role (
-  user_id INT,
-  role_id INT,
-  FOREIGN KEY (user_id) REFERENCES tb_user(id),
-  FOREIGN KEY (role_id) REFERENCES tb_role(id),
-  PRIMARY KEY (user_id, role_id)
+CREATE TABLE IF NOT EXISTS tb_user_role (
+   user_id INT,
+   role_id INT,
+   FOREIGN KEY (user_id) REFERENCES tb_user(id),
+   FOREIGN KEY (role_id) REFERENCES tb_role(id),
+   PRIMARY KEY (user_id, role_id)
 );
